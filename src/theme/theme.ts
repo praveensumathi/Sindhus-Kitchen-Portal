e6a05be776aeb19198890255f14c50f53990d93e
BoobeshPalanisamy <boobeshmalar@gmail.com>
2023-10-20 15:23:09 +0530

    add-structure-folder

---
+++ src/theme/theme.ts
@@ -0,0 +1,26 @@
+import { createTheme } from "@mui/material/styles";
+import { red } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1556fe",
+    },
+    secondary: {
+      main: "#a30200",
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+  typography: {
+    fontFamily: "catamaran-semiBold",
+    button: {
+      textTransform: "none",
+      fontWeight: 500,
+      textDecoration: "none",
+    },
+  },
+});
+
+export default theme;
