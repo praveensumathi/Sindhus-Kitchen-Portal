1bb9bbea5384e3422dd94e611fb62ea99a78275f
sathyaprabaS <sathyapraba48@gmail.com>
2023-10-25 12:39:31 +0530

    serachbar-design

--- src/pages/catering/CateringPage.tsx
+++ src/pages/catering/CateringPage.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
+import SearchBar from "./SearchBar";
 function CateringPage() {
   return (
     <Container>
       <Box>CateringPage</Box>
+      <Box>
+        <SearchBar/>
+      </Box>
     </Container>
   );
 }
---
+++ src/pages/catering/SearchBar.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
+import { Button } from '@mui/material';
+
+
+
+const categoryOptions = [
+  { label: "Snacks" },
+  { label: "BreakFast" },
+  { label: "Veg Main Course" },
+  { label: "Non-Veg Main Course" },
+  { label: "Deserts" },
+];
+const foodOptions = [
+  { label: "Food1" },
+  { label: "Food2" },
+  { label: "Food3" },
+  { label: "Food4" },
+  { label: "Food5" }
+];
+
+
+function SearchBar() {
+
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs>
+        <Autocomplete
+          id="category-autocomplete"
+          options={categoryOptions}
+          getOptionLabel={(option) => option.label}
+          renderInput={(params) => (
+            <TextField {...params} label="Select Category" variant="outlined" />
+          )}
+        />
+      </Grid>
+      <Grid item xs={6}>
+        <Autocomplete
+          id="category-autocomplete"
+          options={foodOptions}
+          getOptionLabel={(option) => option.label}
+          renderInput={(params) => (
+            <TextField {...params} label="Select Food" variant="outlined" />
+          )}
+        />
+      </Grid>
+      <Grid item xs sx={{ display: "flex" }}>
+        <Button variant="outlined">Search</Button>
+        <Button variant="outlined">Clear</Button>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default SearchBar
+
