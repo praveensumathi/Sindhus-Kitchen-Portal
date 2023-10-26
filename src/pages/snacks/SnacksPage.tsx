2f8eca0ab5ea36db03776385b8850161c4de79da
Deepa-Senthil <deepasenthil5812@gmail.com>
2023-10-26 16:40:50 +0530

    common product card add , navbar logo change

--- src/pages/snacks/SnacksPage.tsx
+++ src/pages/snacks/SnacksPage.tsx
@@ -1,10 +1,22 @@
-import React from "react";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
+import CommonProductCard from "../../common/component/CommonProductCard";
+import { productCardList } from "../../seed-data/seed-data";
+
 function SnacksPage() {
   return (
     <Container>
-      <Box>SnacksPage</Box>
+      <Box my={4}>
+        {productCardList.map((product) => (
+          <CommonProductCard
+            key={product.id}
+            title={product.title}
+            mrpprice={product.mrpprice}
+            offerprice={product.offerprice}
+            imageUrl={product.imageUrl}
+          />
+        ))}
+      </Box>
     </Container>
   );
 }
