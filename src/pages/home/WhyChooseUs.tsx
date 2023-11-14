3f2b18099df1a2cfe8cac2daeca5f154bb27c5a6
dharaniya-thiyagarajan <dharaniya2000@gmail.com>
2023-11-14 11:48:00 +0530

    Home page Animation

--- src/pages/home/WhyChooseUs.tsx
+++ src/pages/home/WhyChooseUs.tsx
@@ -2,6 +2,8 @@ import { Box, Typography, Container, Grid, Card } from "@mui/material";
 import { IWhyChooseUs } from "../../interface/types";
 import { whyChooseUsSytle } from "../../styles/WhyChooseUsStyle";
 import { useTheme } from "@mui/material/styles";
+import Fade from "react-reveal/Fade";
+import Bounce from "react-reveal/Bounce";
 
 interface Iprops {
   whyChooseUs: IWhyChooseUs[];
@@ -9,7 +11,6 @@ interface Iprops {
 
 function WhyChooseUs(props: Iprops) {
   const { whyChooseUs } = props;
-
   const classes = whyChooseUsSytle();
   const theme = useTheme();
 
@@ -24,74 +25,77 @@ function WhyChooseUs(props: Iprops) {
           textAlign: "center",
         }}
       >
-        <Typography variant="h4" fontWeight={"bold"} color={"primary"}>
-          WHY SINDHU'S KITCHEN?
-        </Typography>
-        <Typography
-          variant="h5"
-          fontWeight={"bold"}
-          sx={{ opacity: 0.7 }}
-          mt={2}
-        >
-          We make our food as if we are cooking for our family because that is
-          exactly what we are doing !
-          <br />
-          <Box
-            component={"span"}
-            sx={{
-              color: `orange`,
-              opacity: 1,
-            }}
+        <Fade right>
+          <Typography variant="h4" fontWeight={"bold"} color={"primary"}>
+            WHY SINDHU'S KITCHEN?
+          </Typography>
+        </Fade>
+        <Fade left>
+          <Typography
+            variant="h5"
+            fontWeight={"bold"}
+            sx={{ opacity: 0.7 }}
+            mt={2}
           >
-            Welcome to Sindhu’s Kitchen
-          </Box>
-        </Typography>
+            We make our food as if we are cooking for our family because that is
+            exactly what we are doing !
+            <br />
+            <Box
+              component={"span"}
+              sx={{
+                color: `orange`,
+                opacity: 1,
+              }}
+            >
+              Welcome to Sindhu’s Kitchen
+            </Box>
+          </Typography>
+        </Fade>
       </Box>
+
       <Grid container spacing={6} justifyContent="center" alignItems="center">
         {whyChooseUs.map((whychoose, index) => {
           return (
             <Grid item key={index} xs={12} sm={6} md={4} py={1}>
-              <Card
-                className={classes.flipcard}
-                sx={{
-                  boxShadow:
-                    "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
-                }}
-                elevation={0}
-              >
-                <Box className={classes.flipCardInner}>
-                  <Box
-                    className={classes.flipCardFront}
-                    style={{
-                      backgroundImage: `url(${whychoose.image})`,
-                    }}
-                  >
+              <Bounce bottom>
+                <Card
+                  className={classes.flipcard}
+                  elevation={0}
+                >
+                  <Box className={classes.flipCardInner}>
                     <Box
-                      className={`${classes.overlay} ${classes.centerImage}`}
+                      className={classes.flipCardFront}
+                      style={{
+                        backgroundImage: `url(${whychoose.image})`,
+                      }}
                     >
                       <Box
-                        sx={{
-                          fontSize: "6rem",
-                        }}
-                        className={classes.imageWithBorder}
+                        className={`${classes.overlay} ${classes.centerImage}`}
                       >
-                        {whychoose.imageicon}
-                      </Box>
+                        <Box
+                          sx={{
+                            fontSize: "6rem",
+                          }}
+                          className={classes.imageWithBorder}
+                        >
+                          {whychoose.imageicon}
+                        </Box>
 
-                      <Typography gutterBottom variant="h5" fontWeight={500}>
-                        {whychoose.title}
-                      </Typography>
+                        <Typography gutterBottom variant="h5" fontWeight={500}>
+                          {whychoose.title}
+                        </Typography>
+                      </Box>
                     </Box>
+                    <Typography
+                      variant="h5"
+                      p={2}
+                      className={classes.flipCardBack}
+                    >
+                      {whychoose.description}
+                    </Typography>
                   </Box>
-                  <Typography
-                    variant="h5"
-                    p={2}
-                    className={classes.flipCardBack}
-                  >
-                    {whychoose.description}
-                  </Typography>
-                </Box>
-              </Card>
+                </Card>
+              </Bounce>
             </Grid>
           );
         })}
