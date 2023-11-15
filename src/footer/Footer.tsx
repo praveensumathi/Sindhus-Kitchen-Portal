12872dd9e6b5db032f89b8f019abfb55c5a2f922
Deepa-Senthil <deepasenthil5812@gmail.com>
2023-11-15 14:42:35 +0530

    changing ,show catering enquiry form only in cateringpage

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -8,71 +8,15 @@ import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import PhoneIcon from "@mui/icons-material/Phone";
 import AccessTimeFilledIcon from "@mui/icons-material/AccessTimeFilled";
 import { useCommonGridStyle } from "../styles/FooterStyle";
-import { DatePicker } from "@mui/x-date-pickers";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import TextField from "@mui/material/TextField";
 import theme from "../theme/theme";
-import * as yup from "yup";
-import { ICateringEnquiry } from "../interface/types";
-import { createCateringEnquiry } from "../services/api";
-import { EnquiryFormInitialValue } from "../constants/InitialValues";
-import { Controller, useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { useSnackBar } from "../context/SnackBarContext";
-import { SnackbarSeverityEnum } from "../enums/SnackbarSeverityEnum";
-import FormControl from "@mui/material/FormControl";
-import FormHelperText from "@mui/material/FormHelperText";
 import Fade from "react-reveal/Fade";
-import Zoom from "react-reveal/Zoom";
-
-const schema = yup.object().shape({
-  fullName: yup.string().required("Name is required"),
-  email: yup
-    .string()
-    .email("Invalid email address")
-    .required("Email is required"),
-  mobileNumber: yup
-    .string()
-    .required()
-    .typeError("Please enter the MobileNumber")
-    .matches(/^[0-9]{10}$/, "Please enter a valid MobileNumber"),
-  eventDate: yup.string().required("Event date is required"),
-});
+import CateringEnquiryForm from "../common/component/CateringEnquiryForm";
+import { useLocation } from "react-router";
 
 function Footer() {
+  const location = useLocation();
+  const isCateringMenuPage = location.pathname === "/cateringpage";
   const classes = useCommonGridStyle();
-  const { updateSnackBarState } = useSnackBar();
-
-  const {
-    handleSubmit,
-    formState: { errors },
-    register,
-    reset,
-    control,
-  } = useForm<ICateringEnquiry>({
-    resolver: yupResolver(schema),
-    mode: "all",
-    defaultValues: EnquiryFormInitialValue,
-  });
-
-  const onSubmitCateringEnquiry = async (data: ICateringEnquiry) => {
-    try {
-      await createCateringEnquiry(data);
-      updateSnackBarState(
-        true,
-        "Form submitted successfully",
-        SnackbarSeverityEnum.SUCCESS
-      );
-      reset();
-    } catch (error) {
-      updateSnackBarState(
-        true,
-        "Error while submitting the form",
-        SnackbarSeverityEnum.ERROR
-      );
-    }
-  };
 
   return (
     <Box
@@ -216,133 +160,19 @@ function Footer() {
               </Box>
             </Grid>
           </Grid>
-          <Zoom>
-            <Box sx={{ zIndex: 1 }}>
-              <form onSubmit={handleSubmit(onSubmitCateringEnquiry)}>
-                <Grid
-                  container
-                  item
-                  spacing={2}
-                  sx={{
-                    backgroundColor: "white",
-                    borderRadius: 5,
-                    p: 2,
-                  }}
-                >
-                  <Grid item xs={12}>
-                    <Typography
-                      variant="h5"
-                      fontWeight={600}
-                      sx={{ textAlign: "center", color: "black" }}
-                    >
-                      Catering Request Form
-                    </Typography>
-                  </Grid>
-
-                  <Grid item lg={6} xs={12}>
-                    <TextField
-                      label="Full Name *"
-                      fullWidth
-                      variant="outlined"
-                      {...register("fullName")}
-                      error={!!errors.fullName}
-                      helperText={
-                        errors.fullName ? errors.fullName.message : ""
-                      }
-                    />
-                  </Grid>
-                  <Grid item lg={6} xs={12}>
-                    <TextField
-                      label="Email *"
-                      fullWidth
-                      variant="outlined"
-                      {...register("email")}
-                      error={!!errors.email}
-                      helperText={errors.email ? errors.email.message : ""}
-                    />
-                  </Grid>
-                  <Grid item lg={6} xs={12}>
-                    <TextField
-                      label="Mobile Number *"
-                      fullWidth
-                      variant="outlined"
-                      {...register("mobileNumber")}
-                      error={!!errors.mobileNumber}
-                      helperText={
-                        errors.mobileNumber ? errors.mobileNumber.message : ""
-                      }
-                    />
-                  </Grid>
-                  <Grid item lg={6} xs={12}>
-                    <TextField
-                      label="Type Of Event"
-                      fullWidth
-                      variant="outlined"
-                      {...register("typeOfEvent")}
-                    />
-                  </Grid>
-
-                  <Grid item lg={3} xs={12}>
-                    <FormControl fullWidth error={!!errors.eventDate}>
-                      <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <Controller
-                          name="eventDate"
-                          control={control}
-                          render={({ field }) => (
-                            <DatePicker
-                              {...field}
-                              label="Event Date *"
-                              onChange={(date) => field.onChange(date)}
-                              sx={{
-                                width: "100%",
-                                backgroundColor: "white",
-                              }}
-                              format="DD-MM-YYYY"
-                            />
-                          )}
-                        />
-                      </LocalizationProvider>
-                      {!!errors.eventDate && (
-                        <FormHelperText>
-                          {errors.eventDate.message}
-                        </FormHelperText>
-                      )}
-                    </FormControl>
-                  </Grid>
-
-                  <Grid item lg={3} xs={12}>
-                    <TextField
-                      label="Guest Count"
-                      fullWidth
-                      variant="outlined"
-                      type="number"
-                      {...register("guestCount")}
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      label="Message"
-                      fullWidth
-                      variant="outlined"
-                      multiline
-                      rows={3}
-                      {...register("message")}
-                    />
-                  </Grid>
-                  <Grid item xs={12} className={classes.commonGridStyle}>
-                    <Button
-                      type="submit"
-                      sx={{ marginBottom: 1 }}
-                      variant="contained"
-                    >
-                      Send Request
-                    </Button>
-                  </Grid>
-                </Grid>
-              </form>
-            </Box>
-          </Zoom>
         </Grid>
+        {isCateringMenuPage && (
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              zIndex: 2,
+            }}
+          >
+            <CateringEnquiryForm />
+          </Box>
+        )}
       </Container>
     </Box>
   );
