f19ab42184ce52333b9686d9305204fdba7f28ed
BoobeshPalanisamy <boobeshmalar@gmail.com>
2023-11-20 16:02:10 +0530

    snackbar

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ import CategoryProducts from "./pages/diningout/CategoryProduct";
 import Specials from "./pages/specials/Specials";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import SnackBarProvider from "./context/SnackBarContext";
+import CustomSnackBar from "./common/CustomSnackBar";
 
 export const queryClient = new QueryClient();
 
@@ -17,24 +19,27 @@ function App() {
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <HashRouter>
-          <Routes>
-            <Route path={paths.ROOT} element={<Layout />}>
-              <Route index element={<Home />} />
-              <Route path={paths.HOME} element={<Home />} />
-              <Route path={paths.SPECIALS} element={<Specials />} />
-              <Route path={paths.SNACKS} element={<SnacksPage />} />
-              <Route path={paths.CATERING} element={<CateringPage />} />
-              <Route path={paths.DININGOUT} element={<DiningOutPage />} />
-              <Route path={paths.PRODUCTDETAIL} element={<ProductDetail />} />
-              <Route
-                path={paths.PRODUCTSBYCATEGORY}
-                element={<CategoryProducts />}
-              />
-            </Route>
-          </Routes>
-        </HashRouter>
-        <ReactQueryDevtools initialIsOpen={false} />
+        <SnackBarProvider>
+          <HashRouter>
+            <Routes>
+              <Route path={paths.ROOT} element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path={paths.HOME} element={<Home />} />
+                <Route path={paths.SPECIALS} element={<Specials />} />
+                <Route path={paths.SNACKS} element={<SnacksPage />} />
+                <Route path={paths.CATERING} element={<CateringPage />} />
+                <Route path={paths.DININGOUT} element={<DiningOutPage />} />
+                <Route path={paths.PRODUCTDETAIL} element={<ProductDetail />} />
+                <Route
+                  path={paths.PRODUCTSBYCATEGORY}
+                  element={<CategoryProducts />}
+                />
+              </Route>
+            </Routes>
+          </HashRouter>
+          <ReactQueryDevtools initialIsOpen={false} />
+          <CustomSnackBar />
+        </SnackBarProvider>
       </QueryClientProvider>
     </>
   );
